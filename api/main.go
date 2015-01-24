e38fcf783f5630f17289840d5c29080db086129c
Kanokon Chansom <may@odd-e.co.th>
2015-01-24 22:07:18 +0700

    commit channel

--- api/main.go
+++ api/main.go
@@ -11,6 +11,7 @@ import (
 // Declare global variable
 var MapPlayer map[string] int = map[string] int {}
 var channel []*websocket.Conn = make([]*websocket.Conn, 0)
+var golbalNumber string = ""
 
 type T struct {
     Action string
@@ -22,7 +23,7 @@ func randomGopher() (string) {
     var position = []string{"A1","A2","A3",
                             "B1","B2","B3",
                             "C1","C2","C3",}
-    randNumber := rand.Intn(8)
+    randNumber := rand.Intn(9)
     return position[randNumber]
 }
 
@@ -58,18 +59,23 @@ func sentToClient(ws *websocket.Conn) {
         if receiveData.Action == "newPlayer" {
             newPlayer(receiveData.Player)
         }else{
-            winnerAddPoint(receiveData.Player,1)
-        }
-        fmt.Println("Position : "+randomGopher())
-        fmt.Println(channel)
-
-        for _, value := range channel {
-            websocket.JSON.Send(value, MapPlayer)
+            if golbalNumber == receiveData.Position {
+                winnerAddPoint(receiveData.Player,1)
+                golbalNumber = randomGopher()
+                for _, value := range channel {
+                    websocket.JSON.Send(value, MapPlayer)
+                    websocket.JSON.Send(value, golbalNumber)
+                }
+                fmt.Println("Position : " + golbalNumber)
+                fmt.Println(channel)
+            }
         }
     }
 }
 
 func main() {
+    golbalNumber = randomGopher()
+    fmt.Println(golbalNumber)
     http.Handle("/start", websocket.Handler(sentToClient))
     err := http.ListenAndServe(":12345", nil)
     if err != nil {
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 	var ws = new WebSocket("ws://localhost:12345/start");
 
 	ws.onopen = function (){
+			ws.send(JSON.stringify({"Action":"newPlayer", "Position":"A1", "Player":"nong"}));
 			ws.send(JSON.stringify({"Action":"hit", "Position":"A1", "Player":"nong"}));
 	}
 	ws.onmessage = function(msg) {
