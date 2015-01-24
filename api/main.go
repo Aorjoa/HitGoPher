79d8cb905702a8dcea086e134e324180c86bbac7
AorJoa <dekcomgang@hotmail.com>
2015-01-24 16:38:47 +0700

    before change map value

--- api/main.go
+++ api/main.go
@@ -25,6 +25,10 @@ func prepairData(position string, point int) (string) {
     return position + "|" + strconv.Itoa(point)
 }
 
+// func newPlayer(name string) (Player){
+//     PlayerSlice = append(PlayerSlice,)
+// }
+
 func sentToClient(ws *websocket.Conn) {
     //var position = randomGopher()
     //var point = getCurrentPoint()
@@ -32,6 +36,12 @@ func sentToClient(ws *websocket.Conn) {
     var receiveData T
     for {
         websocket.JSON.Receive(ws,&receiveData)
+            if receiveData.Action == "newPlayer" {
+                map[string]interface{}{
+                    "playerName" : receiveData.Player,
+                    "score" : 0
+            }
+        }
         fmt.Println(receiveData.Action)
         fmt.Println(receiveData.Position)
         fmt.Println(receiveData.Player)
@@ -40,6 +50,8 @@ func sentToClient(ws *websocket.Conn) {
 }
 
 func main() {
+    var SlicePlayer [] map[string]interface{} = make([] map[string]interface{},0)
+    fmt.Println(MapPlayer)
     http.Handle("/action", websocket.Handler(sentToClient))
     err := http.ListenAndServe(":12345", nil)
     if err != nil {
