681a757522ba1652ac17d2051ad90ce9a8460265
AorJoa <dekcomgang@hotmail.com>
2015-02-10 21:22:51 +0700

    before revision to old runner

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@
         }
     }
     // Connect WebSocket server
-    var ws = new WebSocket("ws://104.236.3.213:5535/start");
+    var ws = new WebSocket("wss://localhost:5535/start");
 
     ws.onopen = function (){
         $("#statusTxt").text("Status : Connected");
